fb3fbf655a54d08e7f291b65c5fb774134a4cbf7
学生李令新 <stullx@qq.com>
2017-11-06 10:41:21 +0800

    HTTP请求方法枚举。

---
+++ src/http/HttpReqMethod.java
@@ -0,0 +1,20 @@
+/**
+ * @author 李令新 stullx@qq.com
+ * 2017年11月6日
+ */
+package http;
+
+/**
+ * @author 李令新 stullx@qq.com
+ * 2017年11月6日
+ */
+public enum HttpReqMethod {
+	GET,
+	HEAD,
+	POST,
+	PUT,
+	DELETE,
+	CONNECT,
+	OPTIONS,
+	TRACE
+}
